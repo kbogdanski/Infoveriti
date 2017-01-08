dad6bcc4501a8ce9a34636b7efbdafc90d68ff79
kbogdanski <kamil004@op.pl>
2017-01-08 16:40:32 +0100

    delete index.html.twig in app catalog

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,13 +11,10 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template()
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        //return $this->render('default/index.html.twig', array(
-        //    'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        //));
-        return $this->render('default/index.html.twig');
+        return ;
     }
 }
